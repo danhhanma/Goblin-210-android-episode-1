40f4e7470c99ec05d55a1ea60afc0d652bddd2c6
danhhanma <thanhdanhtqd@gmail.com>
2026-01-21 00:15:35 +0700

    variable test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,19 @@
 package org.goblin
 
+fun test(){
+    var email :String = "goblin@gmail.com"
+    email = "cc"
+    println(email)
+}
+
+
+
+
+
 fun main() {
     val name = "Goblin"
-    for(i in 1..10) {
-        println("Goblin episode $i")
-    }
+//    for(i in 1..10) {
+//        println("Goblin episode $i")
+//    }
+    test()
 }
